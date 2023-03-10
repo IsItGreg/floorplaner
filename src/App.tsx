3e9bf867baac55a7f0b765b6be2c5e71a63cac58
Greg Smelkov <gsmelkov@gmail.com>
2023-03-10 00:02:32 -0500

    Added walls

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,26 @@
 import React from "react";
-import logo from "./logo.svg";
 import "./App.css";
 import { Stage, Layer, Rect, Circle } from "react-konva";
 
+// essentially vertices and edges of a graph
 type Corner = {
   x: number;
   y: number;
+  id: number;
+};
+
+type Wall = {
+  corners: Corner[]; // 2 corners
+  thickness: number;
 };
 
 function App() {
   const [creatingRoom, setCreatingRoom] = React.useState(false);
+  const [creatingWall, setCreatingWall] = React.useState(false);
   const [circlePosition, setCirclePosition] = React.useState({ x: 0, y: 0 });
   const [corners, setCorners] = React.useState<Corner[]>([]);
   const [newCorners, setNewCorners] = React.useState<Corner[]>([]);
+  const [walls, setWalls] = React.useState<Wall[]>([]);
 
   const handleMouseMove = (e: any) => {
     const stage = e.target.getStage();
@@ -26,6 +34,11 @@ function App() {
         { ...newCorners[2], x: mousePos.x, y: mousePos.y },
         { ...newCorners[3], y: mousePos.y },
       ]);
+    } else if (creatingWall && newCorners.length === 2) {
+      setNewCorners([
+        newCorners[0],
+        { ...newCorners[1], x: mousePos.x, y: mousePos.y },
+      ]);
     }
   };
 
@@ -33,11 +46,18 @@ function App() {
     const stage = e.target.getStage();
     const mousePos = stage.getRelativePointerPosition();
     if (creatingRoom) {
+      const maxCornerId = Math.max(...corners.map((corner) => corner.id), 0);
       setNewCorners([
-        { x: mousePos.x, y: mousePos.y },
-        { x: mousePos.x, y: mousePos.y },
-        { x: mousePos.x, y: mousePos.y },
-        { x: mousePos.x, y: mousePos.y },
+        { x: mousePos.x, y: mousePos.y, id: maxCornerId + 1 },
+        { x: mousePos.x, y: mousePos.y, id: maxCornerId + 2 },
+        { x: mousePos.x, y: mousePos.y, id: maxCornerId + 3 },
+        { x: mousePos.x, y: mousePos.y, id: maxCornerId + 4 },
+      ]);
+    } else if (creatingWall) {
+      const maxCornerId = Math.max(...corners.map((corner) => corner.id), 0);
+      setNewCorners([
+        { x: mousePos.x, y: mousePos.y, id: maxCornerId + 1 },
+        { x: mousePos.x, y: mousePos.y, id: maxCornerId + 2 },
       ]);
     }
   };
@@ -47,8 +67,23 @@ function App() {
     // const mousePos = stage.getRelativePointerPosition();
     if (creatingRoom) {
       setCorners([...corners, ...newCorners]);
+      setWalls([
+        ...walls,
+        { corners: [newCorners[0], newCorners[1]], thickness: 20 },
+        { corners: [newCorners[1], newCorners[2]], thickness: 20 },
+        { corners: [newCorners[2], newCorners[3]], thickness: 20 },
+        { corners: [newCorners[3], newCorners[0]], thickness: 20 },
+      ]);
       setNewCorners([]);
       setCreatingRoom(false);
+    } else if (creatingWall) {
+      setCorners([...corners, ...newCorners]);
+      setWalls([
+        ...walls,
+        { corners: [newCorners[0], newCorners[1]], thickness: 20 },
+      ]);
+      setNewCorners([]);
+      setCreatingWall(false);
     }
   };
 
@@ -61,11 +96,20 @@ function App() {
           </h1>
           <button
             className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
-            onClick={() => setCreatingRoom(true)}
+            onClick={() => {
+              setCreatingWall(false);
+              setCreatingRoom(true);
+            }}
           >
             <span>New room</span>
           </button>
-          <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+          <button
+            className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+            onClick={() => {
+              setCreatingRoom(false);
+              setCreatingWall(true);
+            }}
+          >
             <span>New wall</span>
           </button>
         </div>
@@ -74,7 +118,7 @@ function App() {
         <Stage
           width={window.innerWidth}
           height={window.innerHeight}
-          draggable={!creatingRoom}
+          draggable={!creatingRoom && !creatingWall}
           className="bg-gray-200"
           onMouseMove={handleMouseMove}
           onMouseDown={handleMouseDown}
@@ -101,6 +145,31 @@ function App() {
                 fill="green"
               />
             ))}
+          </Layer>
+          <Layer>
+            {walls.map((wall, index) => {
+              return (
+                <Rect
+                  key={index}
+                  offsetY={wall.thickness / 2}
+                  x={wall.corners[0].x}
+                  y={wall.corners[0].y}
+                  width={Math.sqrt(
+                    Math.pow(wall.corners[1].x - wall.corners[0].x, 2) +
+                      Math.pow(wall.corners[1].y - wall.corners[0].y, 2)
+                  )}
+                  height={wall.thickness}
+                  fill="black"
+                  rotation={
+                    Math.atan2(
+                      wall.corners[1].y - wall.corners[0].y,
+                      wall.corners[1].x - wall.corners[0].x
+                    ) *
+                    (180 / Math.PI)
+                  }
+                />
+              );
+            })}
           </Layer>
           <Layer>
             {creatingRoom && (
